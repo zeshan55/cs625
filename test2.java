83f80ba60bf3ac1422f142e6cf56c4e9fcf934e3
zeshan55 <48020726+zeshan55@users.noreply.github.com>
2019-03-06 20:06:38 -0800

    commit3

--- test2.java
+++ test2.java
@@ -1,10 +1,14 @@
-public class Test1 {
+public class Test2 {
 public static void main(String[] args) {
 int x=0;
 int y=0;
 while(x<5){
-y=y+x;
-System.out.println(x + "" + y +" ");
+y=y+2;
+if(y>4)
+{
+y=y-1;
+}
+System.out.print(x + "" + y +" ");
 x=x+1;
 }
 }
